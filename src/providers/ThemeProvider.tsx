f3eb6a14236470e39c90b8f755ecc222b5d2ed26
Novo1999 <neptune.navo@gmail.com>
2024-06-13 17:01:31 +0600

    default theme light

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,15 +1,10 @@
 'use client'
-import { LC_THEME_KEY } from '@/utils/constants'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  let localStorageTheme =
-    JSON.parse(localStorage.getItem(LC_THEME_KEY) as string) ?? false
-
-  const [isDark, setIsDark] = useState(localStorageTheme)
   return (
-    <NextThemeProvider attribute='class' defaultTheme='system'>
+    <NextThemeProvider attribute='class' defaultTheme='light'>
       {children}
     </NextThemeProvider>
   )
